ee41db444bb8fb7d05ae14f2929c684a95c1fe4c
CodexYT1 <benjaminferl79@gmail.com>
2025-05-15 22:41:42 +0200

    uyfg

--- EksamenTest.sql
+++ EksamenTest.sql
@@ -1,22 +1,3 @@
-# Use an official base image
-FROM node:16
-
-# Set the working directory
-WORKDIR /app
-
-# Copy package.json and install dependencies
-COPY package.json ./
-RUN npm install
-
-# Copy the rest of the application files
-COPY . .
-
-# Expose the port the app runs on
-EXPOSE 3000
-
-# Start the application
-CMD ["npm", "start"]
-
 
 -- Opprett tabellen for brukere
 CREATE TABLE Users (
